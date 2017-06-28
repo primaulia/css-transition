96d0f1a62c52f964cc7464701d0ea91d67ef88e8
Prima Aulia Gusta <primaulia@gmail.com>
2017-06-28 15:44:21 +0800

    setup circle movement

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ document.addEventListener('DOMContentLoaded', init)
 
 function init () {
   // targeting all the divs
-  var allDivs = document.querySelectorAll('div')
+  // var allDivs = document.querySelectorAll('div')
+  //
+  // allDivs.forEach(function (div) {
+  //   div.addEventListener('mouseover', addExpanded)
+  // })
 
-  allDivs.forEach(function (div) {
-    div.addEventListener('mouseover', addExpanded)
-  })
+  // targeting the circle
+  var circle = document.querySelector('.circle')
+  circle.addEventListener('mouseover', moveCircle)
+}
+
+function randomizer (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function moveCircle (event) {
+  console.log(event)
+  var elem = event.target
+  elem.style.top = randomizer(0, 500) + 'px'
+  elem.style.left = randomizer(0, 500) + 'px'
 }
 
 function addExpanded (event) {
